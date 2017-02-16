8befa4e33ee7dc523353d3c039ae7910e1747c4f
Thomas Chen <chen.thomas.c@gmail.com>
2017-02-15 23:29:01 -0500

    Etude 2-3

--- geom.ex
+++ geom.ex
@@ -1,4 +1,17 @@
 defmodule Geom do
+  @moduledoc """
+  Functions for calculating area of geometric shapes.
+  """
+  @vsn 0.1
+
+  @doc """
+  Calculates area of rectangle, given length and width.
+  Returns product of arguments. Default value for both
+  arguments is 1.
+  """
+
+  @spec area(number, number) :: number
+
   def area(length \\ 1, width \\ 1) do
     length * width
   end
